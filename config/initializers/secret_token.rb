79e7a659907e004b0f08c7e9365bed656b35b7e7
Bartosz Pociecha <bary-1992@o2.pl>
2015-02-10 16:45:13 +0100

    Revert "Od koperkiewicza"
    This reverts commit c491623ffe9516ad4a093c50b6a31c4b97d0cc27.

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Strona::Application.config.secret_token = 'fc113bd91ca1863659199c7030e97608b9e53969d291255e0ec116c0c0d47333bf0678b936ed357831f86627750c854b8a69e4a703fe845809e67f86f3804166'
