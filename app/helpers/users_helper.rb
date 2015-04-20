0cb9bc8fa6693a9c0d6288ef030d801062da2143
PiotrKasperkiewicz <PiotrKasperkiewicz@git.com>
2015-04-21 01:41:50 +0200

    Zrobiona walidacja haseł i avatary

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,7 +3,5 @@ module UsersHelper
     gravatar_id = Digest::MD5::hexdigest(user.mail.downcase)
     gravatar_url = "https://s.gravatar.com/avatar/#{gravatar_id}"
     image_tag(gravatar_url, alt: user.name, class: "gravatar" )
-    end
-
-
+  end
 end
