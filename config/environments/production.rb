f530eba49f9af5deffbe1d988913cc85a26f3368
Araliz <myzzrael@gmail.com>
2015-05-03 17:23:44 +0200

    producion.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,6 @@
 Strona::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
+  ::ActiveSupport::Deprecation.silenced = true
 
   # Code is not reloaded between requests
   config.cache_classes = true
@@ -60,6 +61,7 @@ Strona::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
