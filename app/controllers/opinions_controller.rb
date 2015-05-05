81ede6943c2e5a9dc261c94345558417c5d0dcd0
Jakub Matrac <myzzrael@gmail.com>
2015-05-05 10:40:19 +0200

    Update opinions_controller.rb

--- app/controllers/opinions_controller.rb
+++ app/controllers/opinions_controller.rb
@@ -1,5 +1,5 @@
 class OpinionsController < ApplicationController
-  before_action :logged_in_user, only: [:create, :destroy]
+  #before_action :logged_in_user, only: [:create, :destroy]
   def create
     @opinion = current_user.opnions.build(params[:opinion])
     if @opinion.save
